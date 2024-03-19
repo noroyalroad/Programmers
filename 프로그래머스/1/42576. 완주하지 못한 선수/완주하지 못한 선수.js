b97e280f7a4c00efc5b456c1af775db5e5019fa2
Hyun Kim <zhyunz1@bu.ac.kr>
2024-03-19 12:49:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 100.60 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,16 +1,17 @@
+// 정렬 후 비교하며 같지 않다면 리턴 
 function solution(participant, completion) {
     var answer = '';
     
+    participant.sort() 
+    completion.sort()
     
-    const part = participant.sort()
-    const comp = completion.sort()
-    
-    
-    for(let i =0; i<part.length ; i++){
-        
-        if(part[i]!==comp[i]){
-            return part[i]
+    for(let i =0; i<participant.length;i++){
+        if(participant[i]!==completion[i]){
+            return participant[i]
         }
     }
     
+    console.log(participant, completion)
+    
+    return answer;
 }

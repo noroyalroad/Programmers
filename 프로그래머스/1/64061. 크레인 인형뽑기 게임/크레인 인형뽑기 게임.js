aa604816e9bc5b236bea180ea4a9f8ab23b9915e
Hyun Kim <zhyunz1@bu.ac.kr>
2024-03-20 11:29:59 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 9.01 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,51 +1,58 @@
+
 // [[0,0,0,0,0],
 //  [0,0,1,0,3],
 //  [0,2,5,0,1],
 //  [4,2,4,4,2],
 //  [3,5,1,3,1]]
-const solution =(board, moves) => {
+
+// [3,0] 
+// [1,5,3,5,1,2,1,4]
+// board[i] [j] 를 돌면서 0 아닌 0 원소를 찾는다 
+// j는 moves의 원소가 된다 
+// 찾으면 빈배열에 넣고 그 위치를 0으로 바꾼다 
+function solution(board, moves) {
     var answer = 0;
     let stack = []
     
     moves.forEach(v=>{
         
-        let select = selct(board, v-1) 
+        // let indexdoll = indexs(board,v)
         
-        if(select!==undefined){
+        let dollindex =  indexs(board,v-1) 
+        
+        if(dollindex!==undefined){
+            stack.push(dollindex)
             
-            stack.push(select) 
-            
-            if(stack[stack.length-2] ===stack[stack.length-1]){
-                stack.pop()
-                stack.pop()
+            if(stack[stack.length-1]===stack[stack.length-2]){
+                stack.splice(-2)
                 answer+=2
-                
             }
-            
-            
         }
-       
+        
+        
+        console.log(dollindex)
+        
     })
     
     return answer;
 }
 
-const selct = (board,index) =>{ 
-    
-    
-    for(let i in board){
+let indexs = (board, index) =>{
         
-         if(board[i][index] !== 0) {
-            let selected = board[i][index];
-            board[i][index] = 0;
-
-            return selected;
+        for(let i = 0 ; i< board.length;i++){
+            
+            if(board[i][index]!==0){
+                let indexdoll = board[i][index]
+                board[i][index] =0
+                
+                
+                return indexdoll
+                
+            }
+            
+            
         }
+        
+        
+        
     }
-    
-    
-    
-    
-    
-    
-}

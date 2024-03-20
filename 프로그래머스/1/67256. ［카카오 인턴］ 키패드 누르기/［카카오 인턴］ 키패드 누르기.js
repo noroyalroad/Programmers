b025060e0e2e0f0e3dbe0195e13f1d8f19078fa7
Hyun Kim <zhyunz1@bu.ac.kr>
2024-03-20 11:57:15 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.66 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,74 +1,65 @@
-
+//키패드는 4행 3열  
+// 각 숫자의 위치  
+//1. 왼손과 오른손의 위치를 번호를 누를 때 알 수 있어야 한다 
+// 첫 시작은 왼손은 * 오른손은 # 
+//1 4 7 3 6 9 외에 다른 숫자를 눌렀을 시 현재 왼손의 위치와 오른손의 위치와 다른 숫자의 거리를 계산하여 더 작은 걸로 결정한다 
+// 거리가 같은 경우는 hand에 따라 
 function solution(numbers, hand) {
     
+    let keypad = [ [3,1], [0,0], [0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2] ]   
     
-    
- 
-    
+   
     var answer = '';
-    
-    let keypad = [ [3,1], [0,0],[0,1], [0,2] ,
-                     [1,0],[1,1], [1,2] ,
-                     [2,0],[2,1], [2,2],
-                      ]
-    
-    
-    //왼속 시작 위치 
-    let left  =  [3,0]
-    //오른손 시작 위치 
+     let left = [3,0] 
     let right = [3,2] 
     
     
-    
-    let left_press  =(num) =>{
-        left  = keypad[num] 
-        return answer+='L'
-    }
-    
-    let  right_press =(num)  => {
-        right = keypad[num] 
-        return answer+='R'
-    }
-    
-    numbers.forEach((v,i)=>{
+    for(let x of numbers){
         
         
-        if(v===1 || v===4 || v===7){
-            left_press(v)
+        if(x===1 || x===4 || x===7){
+            answer+='L' 
+            left = keypad[x]
+            
         }
-        else if(v===3 || v===6 || v===9){
-            right_press(v)
+        else if(x===3 || x===6 || x===9){
+            answer+="R" 
+            right = keypad[x]
         }
-        
         else{
-            	// [ 2, 0 ] l  현재 left 위치 
-            	// [ 3, 2 ] r  현재 right 위치 
-            	// [ 3, 1 ]  // 1 4 7   3 6 9 가 아닌 숫자의 위치 
             
-            let left_compare = Math.abs(left[0]-keypad[v][0])+ Math.abs(left[1]-keypad[v][1]) 
-            let right_compare = Math.abs(right[0]-keypad[v][0])+ Math.abs(right[1]-keypad[v][1]) 
-            
-           left_compare < right_compare ? left_press(v) : left_compare > right_compare ? right_press(v) : hand == 'left' ? left_press(v) : right_press(v);
+            let leftdistance  = Math.abs(left[0] - keypad[x][0]) + Math.abs(left[1] - keypad[x][1]);
+            let rightdistance = Math.abs(right[0] - keypad[x][0]) + Math.abs(right[1] - keypad[x][1]);
 
+            
+            
+            
+            if(leftdistance < rightdistance){
+                answer+='L'
+                left = keypad[x]
+            }
+            else if(leftdistance >rightdistance){
+                 answer+='R'
+                 right = keypad[x]
+            }
+            else{
+                if(hand==="right"){
+                    answer+='R'
+                    right = keypad[x]
+                }
+                else{
+                     answer+='L'
+                     left = keypad[x]
+                    
+                }
+            }
             
         }
-    })
-    
-    
-    
-    
-    
-    
-    return answer
-    
-    
-    
-    
-    
-    
-    
-
+        
+        
     }
+
     
-
-
+    
+    return answer;
+}

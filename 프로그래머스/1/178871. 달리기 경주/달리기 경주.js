b259fa4ec58e3f85782f58d65ee2a3075fb0eb91
Hyun Kim <zhyunz1@bu.ac.kr>
2024-03-26 11:49:14 +0900

    [level 1] Title: 달리기 경주, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,38 +1,22 @@
-
+// 현재 플레이어의 랭킹을 만든다 
+// 그 랭킹을 가지고 callings에서 선수이름이 부를 때 마다 인덱스 변경 
 function solution(players, callings) {
     var answer = [];
-
-    // 1)
-    let obj = {}
-    // 2)
-    players.forEach((e, i)=> {
-        obj[e] = (obj[e] |0) + i
-    })
-
-    // 3)
-    for(let i=0; i<callings.length; i++) {
-        // 4)
-        let player_index = obj[callings[i]];
-
-        if(player_index > 0 && player_index != undefined) {
-
-            // 5)
-            // players 배열 변경
-            let temp = players[player_index -1]; // 앞의 값을 temp에 담기
-            players[player_index -1] = players[player_index]; // 현재 값을 앞의 값에 넣기
-            players[player_index] = temp; // temp 값을 현재 값에 넣기
-
-            // 6)
-            // players를 찾기 위한 obj 객체 값도 바뀐대로 변경
-            // ["mumu", "soe", "poe", "kai", "mine"]  ->  { mumu: 0, soe: 1, poe: 2, kai: 3, mine: 4 }
-            // ['mumu', 'soe', 'kai', 'poe', 'mine']  ->  { mumu: 0, soe: 1, poe: 3, kai: 2, mine: 4 }
-            obj[players[player_index -1]] =  player_index -1;
-            obj[players[player_index]] = player_index;
-        }
+    let playerobj = {}
+    players.forEach((v,i)=>{playerobj[v]=i})
+    
+    for(let name of callings){
+        
+       let rank = playerobj[name] 
+       
+       let temp =  players[rank-1] 
+       players[rank-1]  = players[rank]
+        players[rank] = temp 
+        
+        playerobj[players[rank-1] ] = rank -1
+        playerobj[players[rank] ] =rank
+        
     }
-
-    // 7)
-    answer = players;
-
-    return answer;
+    
+    return players;
 }

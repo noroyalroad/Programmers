dbc7728bd1d10368f107d394480dbb5b63614671
Hyun Kim <zhyunz1@bu.ac.kr>
2024-05-23 00:00:49 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n]; 
+       int X =  0;
+        for(int i=  0 ; i< n; i++){
+     
+            
+                  
+           
+            answer[i] = (long) (i+1)*x  ; 
+           
+            
+        }
+        return answer;
+    }
+}

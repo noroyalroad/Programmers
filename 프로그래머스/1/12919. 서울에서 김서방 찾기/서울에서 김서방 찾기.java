be74466dd3a6226455dfac78f72553489fbc4934
Hyun Kim <zhyunz1@bu.ac.kr>
2024-05-23 00:20:07 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 9.47 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,10 @@
+import java.util.* ; 
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        
+        
+        
+        return "김서방은 "+ Arrays.asList(seoul).indexOf("Kim") + "에 있다";
+    }
+}

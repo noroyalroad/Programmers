abf5230edea5a5e614324541c6c2537a8cef1393
Hyun Kim <zhyunz1@bu.ac.kr>
2024-06-06 00:09:55 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 20.37 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()]; 
+        
+        for(int i = 0 ; i< s.length(); i++){
+            
+            
+            String sub = s.substring(0,i) ; 
+            if(sub.indexOf(s.charAt(i)) ==-1 ){
+                answer[i] = -1  ; 
+            }
+            else{
+                answer[i] = i-sub.lastIndexOf(s.charAt(i)) ;
+            }
+            
+        }
+        return answer;
+    }
+}

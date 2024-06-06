084dd498a67f774fcaff8e58a3a82dfe3105351c
Hyun Kim <zhyunz1@bu.ac.kr>
2024-06-06 11:22:42 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.45 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+class Solution {
+    public Set<Integer> solution(int[] numbers) {
+        int[] answer = {};
+        
+        Set<Integer> set = new TreeSet<>() ; 
+        for(int i = 0; i< numbers.length-1; i++){
+            for(int j= i+1 ; j< numbers.length; j++){
+                set.add(numbers[i]+ numbers[j]);
+            }
+        }
+       
+        return set;
+    }
+}

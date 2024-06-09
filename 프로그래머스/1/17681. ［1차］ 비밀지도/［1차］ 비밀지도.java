c80064d02eb1f091f79c7972170edb35ff42919b
Hyun Kim <zhyunz1@bu.ac.kr>
2024-06-09 23:56:10 +0900

    [level 1] Title: [1차] 비밀지도, Time: 8.16 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,11 @@
+class Solution {
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+      String[] answer = new String[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i]));
+            answer[i] = answer[i].replaceAll("1", "#").replaceAll("0", " ");
+        }
+
+        return answer;
+  }
+}
